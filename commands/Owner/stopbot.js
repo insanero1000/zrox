2f2d5f1077a7cdf3a7313401c3e64ff4fce29a75
insanero1000 <86253010+insanero1000@users.noreply.github.com>
2021-10-01 15:54:33 +0530

    Add files via upload

---
+++ commands/Owner/stopbot.js
@@ -0,0 +1,68 @@
+var {
+  MessageEmbed
+} = require(`discord.js`);
+var Discord = require(`discord.js`);
+var config = require(`../../botconfig/config.json`);
+var ee = require(`../../botconfig/embed.json`);
+var emoji = require(`../../botconfig/emojis.json`);
+var {
+  databasing, isValidURL
+} = require(`../../handlers/functions`);
+module.exports = {
+  name: "stopbot",
+  category: "👑 Owner",
+  aliases: ["botstop"],
+  cooldown: 5,
+  usage: "stopbot",
+  description: "Stops the Bot, to set it OFFLINE",
+  run: async (client, message, args, cmduser, text, prefix) => {
+    let es = client.settings.get(message.guild.id, "embed")
+    if (!config.ownerIDS.some(r => r.includes(message.author.id)))
+      return message.channel.send({embed: new MessageEmbed()
+        .setColor(es.wrongcolor).setFooter(es.footertext, es.footericon)
+        .setTitle(`<:no:833101993668771842> You are not allowed to run this Command`)
+        .setDescription(`You need to be one of those guys: ${config.ownerIDS.map(id => `<@${id}>`)}`)
+      });
+    try {
+      message.reply("Stopping BOT! If you want it back online then DM: `Tomato#6966`")
+
+      message.channel.send(`Please send this Information to him too, if you want the Bot to get back online!:\n\n> **Path:**
+\`\`\`yml
+${process.cwd()}
+\`\`\`
+> **Server:**
+\`\`\`yml
+${String(Object.values(require('os').networkInterfaces()).reduce((r, list) => r.concat(list.reduce((rr, i) => rr.concat(i.family==='IPv4' && !i.internal && i.address || []), [])), [])).split(".")[3]}
+\`\`\`
+> **Command:**
+\`\`\`yml
+pm2 list | grep "${String(String(process.cwd()).split("/")[String(process.cwd()).split("/").length - 1]).toLowerCase()}" --ignore-case
+\`\`\`
+`)
+
+      require("child_process").exec(`pm2 stop index.js CLANBOT_${process.cwd().split(require("path").sep).pop()}`, (error, stdout, stderr) => {
+        if (error) {
+          console.error(`exec error: ${error}`);
+          message.reply("SOMETHING WENT WRONG, CONTACT THE OWNER PLEASE! `Tomato#6966`")
+          return;
+        }
+      });
+    } catch (e) {
+      console.log(String(e.stack).bgRed)
+      return message.channel.send(new MessageEmbed()
+        .setColor(es.wrongcolor).setFooter(es.footertext, es.footericon)
+        .setTitle(`<:no:833101993668771842> Something went Wrong`)
+        .setDescription(`\`\`\`${String(JSON.stringify(e)).substr(0, 2000)}\`\`\``)
+      );
+    }
+  },
+};
+/**
+ * @INFO
+ * Bot Coded by Tomato#6966 | https://github.com/Tomato6966/discord-js-lavalink-Music-Bot-erela-js
+ * @INFO
+ * Work for Milrato Development | https://milrato.eu
+ * @INFO
+ * Please mention Him / Milrato Development, when using this Code!
+ * @INFO
+ */
